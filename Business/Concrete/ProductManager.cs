af2308dbe3828c5458d75dd2a3e5c89bcbb5a39d
busenuryildiz <busenuryildiz5337@gmail.com>
2023-11-21 22:10:45 +0300

    Day 7-8

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetAllByUnitsInStock(short stock)
+        {
+            return _productDal.GetAll(p => p.UnitsInStock == stock);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+
+        }
+        
+
+        //customer citysi London olanları getir
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetAllByUnitsInStock(5))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
