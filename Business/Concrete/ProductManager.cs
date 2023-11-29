8c8b8d21e2dc9f753cbc32196004417bb998a3e2
busenuryildiz <busenuryildiz5337@gmail.com>
2023-11-29 20:41:43 +0300

    Add WebAPI

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -20,9 +20,10 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-
+        
         public IResult Add(Product product)
         {
+
             //business codes
             if (product.ProductName.Length < 2)
             {
@@ -35,7 +36,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
